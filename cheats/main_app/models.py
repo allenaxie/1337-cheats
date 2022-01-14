1b5dc4596173079699bca33c0cfa4b7c34aea8cf
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-01-14 14:40:33 -0800

    Create login page and functionality

--- cheats/main_app/models.py
+++ cheats/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Cheatsheet(models.Model):
+    title = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
